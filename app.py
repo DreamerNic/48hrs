a1625a4d2b8c71a0fbe6f69e32d529445f746e90
Jeff Souza <jeffrey_souza@ultimatesoftware.com>
2021-10-18 22:03:13 -0500

    remove ugly

--- app.py
+++ app.py
@@ -6,48 +6,61 @@ from slack_sdk.web import WebClient
 from question_former import QuestionMaker
 
 app = App()
+question_ids_by_channel = {} #key: channel_id, value: dict of question_prompts by their timestamp
 
-question_prompts = {}
 
-def start_question(user_id: str, channel: str, client: WebClient):
-    today_question = QuestionMaker(channel,user_id)
+def create_and_send_question(user_id: str, channel: str, client: WebClient):
+    todays_question = QuestionMaker(channel,user_id)
 
-    message = today_question.get_message_payload()
-
+    message = todays_question.get_message_payload()
     response = client.chat_postMessage(**message)
 
+    todays_question.timestamp = response["ts"]
 
-    today_question.timestamp = response["ts"]
+    if channel not in question_ids_by_channel:
+        question_ids_by_channel[channel] = {}
 
-    if channel not in question_prompts:
-        question_prompts[channel] = {}
-    question_prompts[channel][user_id] = today_question
-
+    question_id = todays_question.timestamp #only unique identifier the api returns
+    question_ids_by_channel[channel][question_id] = todays_question
 
 
 @app.event("reaction_added")
-def update_emoji(event, client):
-    channel_id = event.get("item", {}).get("channel")
+def handle_x_emoji_reaction(event, client):
     user_id = event.get("user")
     reaction = event.get("reaction")
-    print(reaction)
-    if channel_id not in question_prompts or reaction != "x":
+    channel_id = event.get("item", {}).get("channel")
+    question_id = event.get("item", {}).get("ts")
+
+    if channel_id not in question_ids_by_channel or question_id not in question_ids_by_channel[channel_id]:
+        return
+
+    if reaction != "x":
         return
 
     # Get the original tutorial sent.
-    question = question_prompts[channel_id][user_id]
+    question = question_ids_by_channel[channel_id][question_id]
 
-    channel_members = []
+    players = []
     for page in client.conversations_members(channel=channel_id):
-        channel_members = channel_members + page['members']
+        players.append(page['members'])
 
-    question.user_id = random.choice(channel_members)
+    # remove bot and player that opted out
+    players.remove(get_bot_id(client))
+    players.remove(user_id)
+
+    # reroll selected player
+    question.user_id = random.choice(players)
 
     # Get the new message payload
     message = question.get_message_payload()
 
     # Post the updated message in Slack
     updated_message = client.chat_update(**message)
+
+
+def get_bot_id(client):
+    test_response = client.auth_test()
+    return test_response['user_id']
 
 # ============== Message Events ============= #
 # When a user sends a DM, the event type will be 'message'.
@@ -59,20 +72,15 @@ def message(event, client):
     text = event.get("text")
 
     if text and text.lower() == "socks":
-        channel_members = []
+        players = []
         for page in client.conversations_members(channel=channel_id):
-            channel_members = channel_members + page['members']
-        print(channel_members)
-        channel_members.remove(get_bot_id(client))
-        print(channel_members)
-        user_id = random.choice(channel_members)
-        start_question(user_id, channel_id, client)
-    if text and text.lower() == "zzsetup":
-        set_bot_id(client)
+            players.append(page['members'])
 
-def get_bot_id(client):
-    test_response = client.auth_test()
-    return test_response['user_id'] 
+        players.remove(get_bot_id(client))
+
+        todays_player = random.choice(players)
+        create_and_send_question(todays_player, channel_id, client)
+
 
 if __name__ == "__main__":
     logger = logging.getLogger()
